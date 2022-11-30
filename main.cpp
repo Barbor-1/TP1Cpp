6f4277e4ea2a92597aba2e11202c5c21ce04084d
Barbor-1 <n.bareke@gmail.com>
2022-11-30 10:29:49 +0100

    Ajout de date

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+#include <iostream>
+#include "date.h"
+
+
 
 int main(int argc, char const *argv[])
 {
     /* code */
+    date::Date Anniv(9,13);
+    std::cout << date::toString(Anniv) << std::endl;
+
     return 0;
 }
