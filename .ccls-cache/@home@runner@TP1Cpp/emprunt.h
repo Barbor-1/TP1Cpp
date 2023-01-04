9d8d1dcbf41663c9eaae091e1969810c8f77905d
Bareke <54248791+Barbor-1@users.noreply.github.com>
2023-01-04 09:11:08 +0000

    Q8.b Ajout methode searchLivreDispo

---
+++ .ccls-cache/@home@runner@TP1Cpp/emprunt.h
@@ -0,0 +1,21 @@
+#ifndef Emprunt_H
+#define Emprunt_H
+#include "date.h"
+
+namespace bibli{
+	class Emprunt{
+		public:
+		Emprunt(date::Date dateEmprunt = date::Date(1,1,1000), std::string isbn = "0000000000", std::string identifiantLecteur = "00000000");
+		std::string getISBN() const;
+		std::string getIdent() const;
+		void disable();
+
+		private:
+			date::Date _dateEmprunt;
+			std::string _isbn;
+			std::string _identifiantLecteur;
+			bool _actif;
+
+	};
+}
+#endif
